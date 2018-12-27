2cc14dd12590aae56c7dba58bc67337149e97fc7
Amarnath Karthi <97amarnathk@gmail.com>
2018-12-27 11:26:40 +0300

    Add docstrings

--- solver.py
+++ solver.py
@@ -1,7 +1,10 @@
 from Trie import Trie
-import timeit
+import itertools
 
 def load_digram(digram_txt):
+    """
+    Returns digram loaded from the file name `"digram_txt"`
+    """
     digram = []
     digram_file = open(digram_txt, 'r')
     for line in digram_file:
@@ -10,6 +13,9 @@ def load_digram(digram_txt):
     return digram
 
 def word_freq(word):
+    """
+    Returns frequency of all alphabets in `word`
+    """
     freq = [0]*26
     for ch in word:
         freq[ord(ch)-ord('a')]+=1
@@ -22,6 +28,10 @@ def word_possible(word_freq, digram_freq):
     return True
 
 def get_word_list(full_word_list_txt, digram):
+    """
+    Returns a list of all words that can possibly be created 
+    on the basis of the frequency count of the `digram`
+    """
     #calculate digram frequency
     digram_str = []
     for row in digram:
@@ -38,47 +48,43 @@ def get_word_list(full_word_list_txt, digram):
     return word_list
 
 def dfs_recursive(row, col, digram, combination, vis, combination_set, trie):
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
+    for i,j in itertools.product([-1, 0, 1], repeat=2):
             next_row = row + i
             next_col = col + j
-
             # out of range
             if next_row not in range(len(digram)) or next_col not in range(len(digram[0])):
                 continue
             # if already visited
             if vis[next_row][next_col]:
                 continue
-
             # if word with the combination as the prefix does not exist
             if not trie.find_prefix(combination)[0]:
                 continue
-            
             #if the combination is the valid word
             if trie.find_prefix(combination)[2]:
                 combination_set.add(combination)
-            
-            vis[next_row][next_col] = 1
+            #mark as visited and continue the search
+            vis[next_row][next_col] = True
             combination += digram[next_row][next_col]
             dfs_recursive(next_row, next_col, digram, combination, vis, combination_set, trie)
+            #unmark
             combination = combination[:-1]
-            vis[next_row][next_col] = 0
+            vis[next_row][next_col] = False
             
 
 def begin_dfs(digram, trie):
-    vis = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    """
+    Returns a set of all words that can be created using the `digram`, 
+    using `trie` as the dictionary of all plausible words
+    """
+    vis = [[False]*4 for i in range(4)]
     combination_set = set()
-    for i in range(len(digram)):
-        for j in range(len(digram[0])):
+    for i,j in itertools.product(range(4), repeat=2):
             combination = ""+digram[i][j]
             vis[i][j] = 1
             dfs_recursive(i, j, digram, combination, vis, combination_set, trie)
             vis[i][j] = 0
-    lst = list(combination_set)
-    lst.sort(key = lambda s: len(s))
-    for w in lst:
-        print(w)
-    print(len(lst))
+    return combination_set
 
 if __name__ == "__main__":
     digram_file_name = "digram.txt"
@@ -88,4 +94,5 @@ if __name__ == "__main__":
     digram = load_digram(digram_file_name)
     word_list = get_word_list(full_word_list, digram)
     trie.add_word_list(word_list)
-    begin_dfs(digram, trie)
+    solution_list = list(begin_dfs(digram, trie))
+    print(solution_list)
